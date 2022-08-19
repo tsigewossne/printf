f57db8d57dae5acc3abc79c452751907e6372e3b
tsigewossne <tsigedejen@gmail.com>
2022-08-19 10:06:12 +0300

    gitignore

--- main.h
+++ main.h
@@ -1,37 +1,41 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
+int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_nbr(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int _putchar(char c);
+int _puts(char *str, int ascii);
+int _nbr_len(int prmNumber);
+int _strlen_recursion(char *s);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_rot13(char *str);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+char *convert_base_pointer(unsigned long p);
 
 /**
- * struct test - defines a structure for symbols and functions
- *
- * @sym: The operator
- * @f: The function associated
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
  */
-struct test
+
+typedef struct flags_printf
 {
-  char *sym;
-
-  int (*f)(va_list);
-};
-typedef struct test test_t;
-
-/* Main functions */
-int parser(const char *format, test_t f_list[], va_list arg_list);
-int _printf(const char *format, ...);
-int _write_char(char c);
-int print_char(va_list list);
-int print_string(va_list list);
-int print_percent(va_list list);
-int print_integer(va_list list);
-int print_number(va_list list);
-
-/*Helper functions*/
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-
+char *c;
+int (*f)(va_list);
+} flags_p;
 #endif
